231128b40c738650dc92d2e8364f837502c0d8c1
Chinmaya Kumar Mishra <imchinu17@gmail.com>
2025-01-30 18:44:38 +0530

    update

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import PdfViewer from '@/components/PDFViewer'
-const page = () => {
-  const pdfUrl="/dummy.pdf";
+import React from 'react';
+import PdfViewer from '@/components/PdfViewer';
+
+const Page = () => {
+  const pdfUrl = "/dummy.pdf";
 
   return (
-    <>
-      <div>page</div>
-      <PdfViewer pdfUrl={pdfUrl}/>
-    </>
-  )
-}
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 py-10">
+      <div className="text-4xl font-semibold text-gray-800 mb-6">PDF Highlighter</div>
+      <div className="w-full max-w-4xl bg-white p-6 rounded-lg shadow-lg">
+        <PdfViewer pdfUrl={pdfUrl} />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default Page;
